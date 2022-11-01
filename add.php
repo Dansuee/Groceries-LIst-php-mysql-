16d4e40f65bc08fba4cc7fb5c020bb65b16db48d
dandan123 <dandan.navales@gmail.com>
2022-11-01 15:03:49 -0700

    add errors

--- add.php
+++ add.php
@@ -1,20 +1,30 @@
 <?php 
 
+$errors = array('item'=>'', 'quantity'=>'', 'price'=>'');
 
 if(isset($_POST['submit'])){
 
     //item name
     if(empty($_POST['item'])){
-        echo 'name item is required <br />';
+        $errors['item'] = 'name item is required <br />';
     } else {
         $item = $_POST['item'];
         //if(preg_match('/^[a-zA-Z\s]+$/', $item)){
-            echo htmlspecialchars($_POST['item']);
+           echo htmlspecialchars($_POST['item']);
     }
      
+     //item price
+     if(empty($_POST['quantity'])){
+        $errors['quantity'] =  'enter item quantity <br />';
+    } else {
+       $price = $_POST['quantity'];
+       echo htmlspecialchars($_POST['quantity']);
+       }
+    
+
     //item price
     if(empty($_POST['price'])){
-        echo 'enter item price <br />';
+        $errors['price'] =  'enter item price <br />';
     } else {
        $price = $_POST['price'];
        echo htmlspecialchars($_POST['price']);
@@ -35,8 +45,13 @@ if(isset($_POST['submit'])){
         <form action="add.php" class="white" method="POST">
             <label>Item:</label>
             <input type="text" name="item">
-            <label>Price:</label>
+            <div class="red-text"><?php echo $errors['item']?></div>
+            <label>Quantity:</label>
+            <input type="text" name="quantity">
+            <div class="red-text"><?php echo $errors['quantity']?></div>
+            <label>Price(php):</label>
             <input type="text" name="price">
+            <div class="red-text"><?php echo $errors['price']?></div>
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
             </div>
