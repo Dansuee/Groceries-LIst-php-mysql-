c1d049d1fd82d06c8fe4054491ff6a94af364137
dandan123 <dandan.navales@gmail.com>
2022-11-01 18:43:24 -0700

    connected to db

--- add.php
+++ add.php
@@ -1,5 +1,6 @@
 <?php 
 
+$item = $quantity = $price = '';
 $errors = array('item'=>'', 'quantity'=>'', 'price'=>'');
 
 if(isset($_POST['submit'])){
@@ -13,7 +14,7 @@ if(isset($_POST['submit'])){
            echo htmlspecialchars($_POST['item']);
     }
      
-     //item price
+     //item quantity
      if(empty($_POST['quantity'])){
         $errors['quantity'] =  'enter item quantity <br />';
     } else {
@@ -29,7 +30,11 @@ if(isset($_POST['submit'])){
        $price = $_POST['price'];
        echo htmlspecialchars($_POST['price']);
        }
-    
+    if(array_filter($errors)){
+       // echo 'error in the form';
+    }else {
+        header('Location: index.php');
+    }
 }
 
 ?>
@@ -44,13 +49,13 @@ if(isset($_POST['submit'])){
         <h4 class="center">Add a Groceries</h4>
         <form action="add.php" class="white" method="POST">
             <label>Item:</label>
-            <input type="text" name="item">
+            <input type="text" name="item" value="<?php echo $item ?>">
             <div class="red-text"><?php echo $errors['item']?></div>
-            <label>Quantity:</label>
-            <input type="text" name="quantity">
+            <label>Quantity(kg/L/pc):</label>
+            <input type="text" name="quantity" value="<?php echo $quantity ?>">
             <div class="red-text"><?php echo $errors['quantity']?></div>
             <label>Price(php):</label>
-            <input type="text" name="price">
+            <input type="text" name="price" value="<?php echo $price ?>">
             <div class="red-text"><?php echo $errors['price']?></div>
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<?php ?>
+<?php 
+
+//connect to db
+$connect = mysqli_connect('localhost', 'root' , '' , 'groceries_list');
+
+//check connection
+if(!$connect){
+    echo 'Connection Error:' . mysqli_connect_error(); 
+}
+
+//query for get all groceries list
+$sql = 'SELECT item, quantity, price FROM grocery';
+
+//get result from db
+$result = mysqli_query($connect, $sql);
+
+//fetch result rows as array
+$groceries = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//free result from memory
+mysqli_free_result($result);
+
+//close connection
+mysqli_close($connect);
+
+print_r($groceries);
+?>
 
 
 <!DOCTYPE html>
